a3b038dd03f0908f31a15151c8b60e60cef1db9c
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-11-04 14:51:43 +0300

    create util for running sync tasks in async executor

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+import asyncio
+from functools import partial, wraps
+
+
+def wrap_sync(func):
+    @wraps(func)
+    async def run(*args, loop=None, executor=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        partial_func = partial(func, *args, **kwargs)
+        return await loop.run_in_executor(executor, partial_func)
+
+    return run
